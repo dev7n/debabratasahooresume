26a3a11ae19467e802484c43ac235dc12bacf158
debabrata sahoo <debabrata@kali.debabrata>
2023-12-14 01:50:24 +0530

    modified index file,css file and added image folder

--- main.js
+++ main.js
@@ -60,3 +60,19 @@ document.querySelector('.btn').addEventListener('click', (evt) => {
       evt.target.classList.remove('loading')
     }, 3000);
   })
+
+
+
+  var btnContainer = document.getElementById("navbar");
+
+  // Get all buttons with class="btn" inside the container
+  var btns = btnContainer.getElementsByClassName("btn");
+  
+  // Loop through the buttons and add the active class to the current/clicked button
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
